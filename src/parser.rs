7769956f2a9ae9bfc0a3e7d663fce6f20307faf2
Carson <crkruege@gmail.com>
2023-12-24 17:30:41 -0700

    added unary function, simplified primary function

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,7 @@
+use core::panic;
+
 use crate::{
-    ast::{Expr, Literal},
+    ast::{Expr, Literal, UnaryOp},
     token::{token_type::TokenType, Token},
 };
 
@@ -18,6 +20,13 @@ pub struct Parser<'a> {
     cur_idx: usize,
 }
 
+/// expression     → equality ;
+/// equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+/// comparison     → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+/// term           → factor ( ( "-" | "+" ) factor )* ;
+/// factor         → unary ( ( "/" | "*" ) unary )* ;
+/// unary          → ( "!" | "-" ) unary | primary ;
+/// primary        → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
 impl<'a> Parser<'a> {
     pub fn new(tokens: &'a Vec<Token>) -> Self {
         Self { tokens, cur_idx: 0 }
@@ -32,8 +41,8 @@ impl<'a> Parser<'a> {
         self.cur_idx += 1;
     }
     /// consumes and advances IF current token matches token_type argument. Returns true if successfully consumed token.
-    fn consume_match(&mut self, token_type: &TokenType) -> bool {
-        let bool = &self.peek().token_type == token_type;
+    fn consume_match(&mut self, token_type: TokenType) -> bool {
+        let bool = self.peek().token_type == token_type;
         if bool {
             self.advance();
         }
@@ -51,35 +60,58 @@ impl<'a> Parser<'a> {
     fn expression(&mut self) -> Expr {
         unimplemented!()
     }
-    fn primary(&mut self) -> Result<Expr, ()> {
-        let types = [
-            TokenType::Num,
-            TokenType::Nil,
-            TokenType::True,
-            TokenType::False,
-        ];
+    fn unary(&mut self) -> Expr {
+        let types = [TokenType::Minus, TokenType::Bang];
         if self.consume_first_match(&types) {
-            let t = self.previous();
-            match t.token_type {
-                TokenType::Num => Ok(Expr::LiteralExpr(Literal::Float(
-                    t.lexeme.parse::<f32>().unwrap(),
-                ))),
-                TokenType::Nil => Ok(Expr::LiteralExpr(Literal::Nil)),
-                TokenType::True => Ok(Expr::LiteralExpr(Literal::Bool(true))),
-                TokenType::False => Ok(Expr::LiteralExpr(Literal::Bool(false))),
-                _ => Err(()),
+            if let Ok(op) = UnaryOp::try_from(&self.previous().token_type) {
+                let rhs = Box::new(self.unary());
+                return Expr::Unary { op, rhs };
             }
+        }
+
+        match self.primary() {
+            Ok(p) => p,
+            Err(e) => panic!("Invalid primary token"),
+        }
+    }
+    fn primary(&mut self) -> Result<Expr, ()> {
+        // let types = [
+        //     TokenType::Num,
+        //     TokenType::Nil,
+        //     TokenType::True,
+        //     TokenType::False,
+        // ];
+        // if self.consume_first_match(&types) {
+        //     let t = self.previous();
+        //     match t.token_type {
+        //         TokenType::Num => Ok(Expr::LiteralExpr(Literal::Float(
+        //             t.lexeme.parse::<f32>().unwrap(),
+        //         ))),
+        //         TokenType::Nil => Ok(Expr::LiteralExpr(Literal::Nil)),
+        //         TokenType::True => Ok(Expr::LiteralExpr(Literal::Bool(true))),
+        //         TokenType::False => Ok(Expr::LiteralExpr(Literal::Bool(false))),
+        //         _ => Err(()),
+        //     }
+        // } else {
+        //     Err(())
+        // }
+
+        if self.consume_match(TokenType::Num) {
+            let t = self.previous();
+            Ok(Expr::LiteralExpr(Literal::Float(
+                t.lexeme.parse::<f32>().unwrap(),
+            )))
+        } else if self.consume_match(TokenType::Nil) {
+            let t = self.previous();
+            Ok(Expr::LiteralExpr(Literal::Nil))
+        } else if self.consume_match(TokenType::True) {
+            let t = self.previous();
+            Ok(Expr::LiteralExpr(Literal::Bool(true)))
+        } else if self.consume_match(TokenType::False) {
+            let t = self.previous();
+            Ok(Expr::LiteralExpr(Literal::Bool(false)))
         } else {
             Err(())
         }
     }
 }
-
-// impl Parsable for Parser<'_> {
-//     type OutOk = Vec<Stmt>;
-//     type OutErr = ParseError;
-//     // will return a list of statements
-//     fn parse() -> Result<Self::OutOk, Self::OutErr> {
-//         unimplemented!()
-//     }
-// }
