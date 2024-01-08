4e3b1a633774427f4d55d71a6db69779c4053dcb
carsonkrueger <crkruege@gmail.com>
2024-01-07 18:00:45 -0700

    fixing directive struct

--- src/asm/directive.rs
+++ src/asm/directive.rs
@@ -1,25 +1,31 @@
-pub enum DirectiveType {
-    Byt,
-    Int,
-    Str,
-}
+use crate::asm::token::{Token, TokenType};
+use std::io::{Seek, Write};
 
 pub enum Directive {
-    Byt1(u8),
-    Byt4(i32),
-    Int1(u8),
-    Int4(i32),
+    Byt(u8),
+    Int(i32),
     Str(String),
 }
 
 impl Directive {
-    fn write(&self) {
+    fn write<W: Write + Seek>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.seek(std::io::SeekFrom::End(0))?;
         let bytes = match &self {
-            Directive::Int4(i) => i.to_le_bytes().as_slice().to_owned(),
-            Directive::Int1(i) => i.to_le_bytes().as_slice().to_owned(),
-            Directive::Byt4(i) => i.to_le_bytes().as_slice().to_owned(),
-            Directive::Byt1(i) => i.to_le_bytes().as_slice().to_owned(),
+            Directive::Int(i) => i.to_le_bytes().as_slice().to_owned(),
+            Directive::Byt(b) => b.to_le_bytes().as_slice().to_owned(),
             Directive::Str(s) => s.as_bytes().to_owned(),
         };
+        writer.write_all(&bytes)?;
+        Ok(())
+    }
+}
+
+impl TryFrom<Token> for Directive {
+    type Error = ();
+    fn try_from(value: Token) -> Result<Self, Self::Error> {
+        match value.token_type {
+            TokenType::BytDir => {}
+            TokenType::IntDir => TokenType::StrDir,
+        }
     }
 }
